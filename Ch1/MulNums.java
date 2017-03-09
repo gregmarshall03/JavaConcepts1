1d8d70609daaf6a40f55c005f9f214b0ba4c1627
gregmarshall03 <greg.marshall7@gmail.com>
2017-03-09 09:13:41 -0600

    Add files via upload

---
+++ Ch1/MulNums.java
@@ -0,0 +1,14 @@
+
+public class MulNums {
+	int num1, num2;
+
+	public MulNums(int a, int b) {
+		this.num1 = a;
+		this.num2 = b;
+		int result = a * b;
+		System.out.println(result);
+		
+		
+	}
+
+}
